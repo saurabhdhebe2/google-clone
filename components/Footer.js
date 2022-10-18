4ba92886c8a776ddd4878098b3871f6f5d04ecb8
saurabhdhebe2 <108879055+saurabhdhebe2@users.noreply.github.com>
2022-10-18 13:23:50 +0530

    get the term from input & redirect to related page

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6">
+    <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600s">
       Copyright &copy; {new Date().getFullYear()} Saurabh Dhebe{" "}
     </footer>
   );
